afb3465156a9ff33eba910c10aa0447c09fdc605
ovlack <steelgseries@hotmail.com>
2025-07-18 08:43:04 -0500

    style: add team background image mapping for 2025 season

---
+++ src/utils/teamBgMap.ts
@@ -0,0 +1,23 @@
+import williamsBG from "../assets/component/teamCard-williams.png";
+import ferrariBG from "../assets/component/teamCard-ferrari.png";
+import kickSauberBG from "../assets/component/teamCard-kick-sauber.png";
+import mclarenBG from "../assets/component/teamCard-mclaren.png";
+import mercedesBG from "../assets/component/teamCard-mercedes.png";
+import astonMartinBG from "../assets/component/teamCard-aston-martin.png";
+import haasBG from "../assets/component/teamCard-haas.png";
+import alpineBG from "../assets/component/teamCard-alpine.png";
+import racingBullsBG from "../assets/component/teamCard-racing-bulls.png";
+import redBullBG from "../assets/component/teamCard-red-bull.png";
+
+export const teamBgMap: Record<string, any> = {
+  McLaren: mclarenBG,
+  Ferrari: ferrariBG,
+  Mercedes: mercedesBG,
+  Williams: williamsBG,
+  "Red Bull Racing": redBullBG,
+  "Kick Sauber": kickSauberBG,
+  "Racing Bulls": racingBullsBG,
+  "Aston Martin": astonMartinBG,
+  Haas: haasBG,
+  Alpine: alpineBG,
+};
